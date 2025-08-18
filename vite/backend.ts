f7f2e78ccd17cfa2e95ca5faa11bdb01490c9dce
Seya <31957516+sglkc@users.noreply.github.com>
2025-08-18 19:01:48 +0700

    feat(vite): change require with import

---
+++ vite/backend.ts
@@ -0,0 +1,47 @@
+import { Plugin, PLUGIN_OPTIONS } from '@motion-canvas/vite-plugin';
+import fs from 'fs';
+
+export default function MultiTrackPlugin(): Plugin {
+  return {
+    name: 'multi-track-audio-files',
+
+    configureServer(server) {
+      server.middlewares.use('/audios', (req, res) => {
+        fs.readdir("./audio", { withFileTypes: true }, (err, files) => {
+          if (err) {
+            res.end(JSON.stringify({ error: "error" }))
+            return
+          }
+          else {
+            const list = files.map(f => f.name)
+            res.end(JSON.stringify(list));
+          }
+        })
+      });
+
+      server.middlewares.use('/cleanup', (req, res) => {
+        fs.stat('./audio/multi-track-audio.wav', function (err, stats) {
+
+          if (err) {
+            res.end(JSON.stringify({ error: "error" }))
+            return
+          }
+
+          fs.unlink('./audio/multi-track-audio.wav', function (err) {
+            if (err) {
+              res.end(JSON.stringify({ error: "error" }))
+              return
+            }
+            
+            res.end(JSON.stringify({ success: "file deleted successfully" }))
+
+          });
+        });
+      });
+    },
+
+    [PLUGIN_OPTIONS]: {
+      entryPoint: '',
+    },
+  };
+}
