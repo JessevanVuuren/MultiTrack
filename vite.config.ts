b410aa52ae4440607e43339b3abfcf2a8389bffa
Seya <31957516+sglkc@users.noreply.github.com>
2025-08-18 21:33:50 +0700

    build: setup vite typescript build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: {
@@ -12,12 +18,15 @@ export default defineConfig({
         'editor-plugin': resolve(__dirname, './src/index.tsx'),
         'vite-plugin': resolve(__dirname, './src/vite/backend.ts'),
       },
+      formats: ['es'],
     },
     rollupOptions: {
       external: [
+        'node:fs/promises',
         '@motion-canvas/2d',
         '@motion-canvas/core',
         '@motion-canvas/vite-plugin',
+        'preact',
       ],
     },
   },
