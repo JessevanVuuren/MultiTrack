3d86244336dd1123c9e043aef7337a6c03bc4be8
Seya <31957516+sglkc@users.noreply.github.com>
2025-08-19 23:43:13 +0700

    build(npm): rename package

--- src/vite/backend.ts
+++ src/vite/backend.ts
@@ -32,7 +32,7 @@ export default function MultiTrackPlugin(): Plugin {
       });
     },
     [PLUGIN_OPTIONS]: {
-      entryPoint: 'multitrack/exporter',
+      entryPoint: 'motion-canvas-multitrack/exporter',
     },
   };
 }
